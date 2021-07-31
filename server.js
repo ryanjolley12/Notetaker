ba6bf67992315615ae4c85fa8b2baad6525defa5
ryanjolley12 <ryanswims@gmail.com>
2021-07-31 10:21:16 -0400

    altered json testing

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 // ----------------------------------------------------------------------------------
 // GET routes
 
